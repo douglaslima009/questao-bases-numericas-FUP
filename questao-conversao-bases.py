39fa20a06caaa36b062f1de869d029ac5cf9ed8f
Douglas Lima <100093979+douglaslima009@users.noreply.github.com>
2023-05-18 22:36:31 -0300

    Update questao-conversao-bases.py

--- questao-conversao-bases.py
+++ questao-conversao-bases.py
@@ -1,42 +1,54 @@
 #Aluno: Douglas Lima Menezes
 #Engenharia de software 2023.1
 
-def dectobin(decimal):
-    return bin(decimal)[2:]
-def bintodec(binario):
-    return int(binario, 2)
-def dectohex(decimal):
-    return hex(decimal)[2:].upper()
+print("Menu:")
+print("1 - Converta Decimal para Binário.")
+print("2 - Converta Binário para Decimal.")
+print("3 - Converta Decimal em Hexadecimal.")
+print("0 - Encerrar o programa.")
 
-def exibir_menu():
+opcao = input("Escolha uma opção válida: ")
+while opcao != "0":
+    if opcao == "1":
+        dec = int(input("Digite um número decimal: "))
+        decorigem = dec
+        bin = ""
+        while dec > 0:
+            resto = dec % 2
+            bin = str(resto) + bin
+            dec = dec // 2
+        print(f"O número decimal {decorigem} corresponde ao número binário: {bin}")
+    elif opcao == "2":
+        bin = int(input("Digite um número binário: "))
+        binorigen = bin
+        dec = 0
+        exp = 0
+        while bin > 0:
+            resto = bin % 10
+            dec = dec + resto * (2 ** exp)
+            bin = bin // 10
+            exp = exp + 1
+        print(f"O número binário {binorigen} corresponde ao número decimal: {dec}")
+    elif opcao == "3":
+        dec = int(input("Digite um número decimal: "))
+        decorigem = dec
+        hexloc = {10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+        hex = ""
+        while dec > 0:
+            resto = dec % 16
+            if resto >= 10:
+                resto = hexloc[resto]
+            hex = str(resto) + hex
+            dec = dec // 16
+        print(f"O número decimal {decorigem} corresponde ao número hexadecimal: {hex}")
+    else:
+        print("O número digitado não é uma opção válida. Por favor, digite novamente.")
+    
     print("Menu:")
-    print("1 - Conversão de Decimal para Binário")
-    print("2 - Conversão de Binário para Decimal")
-    print("3 - Conversão de Decimal em Hexadecimal")
-    print("0 - Encerrar")
+    print("1 - Converta Decimal para Binário")
+    print("2 - Converta Binário para Decimal")
+    print("3 - Converta Decimal em Hexadecimal")
+    print("0 - Encerrar o programa")
 
-def main():
-    while True:
-        exibir_menu()
-        opcao = input("Escolha uma opção: ")
-
-        if opcao == "1":
-            dec = int(input("Digite um número decimal: "))
-            bin = dectobin(dec)
-            print(f"O número decimal {dec} em binário corresponde a: {bin}")
-        elif opcao == "2":
-            bin = input("Digite um número binário: ")
-            dec = bintodec(bin)
-            print(f"O número binário {bin} em decimal corresponde a: {dec}")
-        elif opcao == "3":
-            dec = int(input("Digite um número decimal: "))
-            hex = dectohex(dec)
-            print(f"O número decimal {dec} em hexadecimal corresponde a: {hex}")
-        elif opcao == "0":
-            print("Encerrado.")
-            break
-        else:
-            print("Não foi possível realizar esta operação. Por favor, digite uma opção válida.")
-
-if __name__ == "__main__":
-    main()
+    opcao = input("Escolha uma opção válida: ")
+print("Encerrado.")
